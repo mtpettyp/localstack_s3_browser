c5c3f82f3345cf80da374e5baa250d2f457cfd5b
Mike Pettypiece <mike@pettypiece.ca>
2026-01-07 14:44:44 -0500

    Fix build warnings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,14 @@
 rootProject.name = "localstack-s3-browser"
+
+// Configure toolchain download repositories to avoid deprecation warning
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+// Add toolchain repositories for auto-provisioning
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
